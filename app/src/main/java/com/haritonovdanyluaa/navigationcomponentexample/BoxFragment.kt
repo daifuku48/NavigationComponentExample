2ac357f611e7e2dde5e2c39cd1740fd20809fede
danil <natalya.haritonov.72@gmail.com>
2023-05-08 23:16:21 +0300

    basic navigation

--- app/src/main/java/com/haritonovdanyluaa/navigationcomponentexample/BoxFragment.kt
+++ app/src/main/java/com/haritonovdanyluaa/navigationcomponentexample/BoxFragment.kt
@@ -1,8 +1,43 @@
 package com.haritonovdanyluaa.navigationcomponentexample
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.haritonovdanyluaa.navigationcomponentexample.databinding.FragmentBoxBinding
+import com.haritonovdanyluaa.navigationcomponentexample.databinding.FragmentMainBinding
+import kotlin.random.Random
 
 class BoxFragment : Fragment(R.layout.fragment_box) {
+
+    private var _binding : FragmentBoxBinding? = null
+    private val binding
+        get() = _binding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentBoxBinding.bind(view)
+        val color = requireArguments().getInt(COLOR)
+        binding?.root?.setBackgroundColor(color)
+
+        binding?.goBackButton?.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding?.openSecretButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_boxFragment_to_secretFragment)
+        }
+        binding?.generateNumberButton?.setOnClickListener {
+            val number = Random.nextInt(0, 1000)
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("random", number)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     companion object{
         const val COLOR = "color"
     }
--- app/src/main/java/com/haritonovdanyluaa/navigationcomponentexample/RootFragment.kt
+++ app/src/main/java/com/haritonovdanyluaa/navigationcomponentexample/RootFragment.kt
@@ -3,6 +3,7 @@ package com.haritonovdanyluaa.navigationcomponentexample
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -26,12 +27,14 @@ class RootFragment : Fragment(R.layout.activity_main) {
         binding?.yellowButton?.setOnClickListener {
             openBox(Color.GREEN)
         }
+
+        val number = findNavController().currentBackStackEntry?.savedStateHandle?.get<Int>("Random")
+        Toast.makeText(requireContext(), number.toString(), Toast.LENGTH_LONG).show()
     }
 
     private fun openBox(color: Int)
     {
         findNavController().navigate(R.id.action_rootFragment2_to_boxFragment, bundleOf(BoxFragment.COLOR to color))
-
     }
 
     override fun onDestroy() {
